55f329fefc66947b555a962e2bfe194f0f3b110f
Jim Hester <james.f.hester@gmail.com>
2019-06-10 13:25:15 -0400

    Try publishing test results

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
 library(azuretest)
 
-test_check("azuretest")
+test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
