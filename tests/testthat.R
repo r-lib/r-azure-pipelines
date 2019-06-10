b294b08211e059e53b1e15be25f4eb8bab423ef4
Jim Hester <james.f.hester@gmail.com>
2019-06-10 13:38:31 -0400

    Conditional check for xml2

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,8 @@
 library(testthat)
 library(azuretest)
 
-test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
+if (require("xml2")) {
+  test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
+} else {
+  test_check("azuretest")
+}
