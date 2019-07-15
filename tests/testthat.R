12146c9a41b649c9e96045d1b9bb3cff2b84b351
Jim Hester <james.f.hester@gmail.com>
2019-07-15 13:12:59 +0200

    Do not try to do a xml report on windows
    It is failing there for unknown reasons

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,9 @@
 library(testthat)
 library(azuretest)
 
-if (require("xml2")) {
+is_windows <- identical(tolower(Sys.info()[["sysname"]]), "windows")
+
+if (!is_windows && require("xml2")) {
   test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
 } else {
   test_check("azuretest")
