881456861d9e369828b3c1e7abc971ac2b4ab18c
Jim Hester <james.f.hester@gmail.com>
2019-08-02 12:02:26 -0400

    Remove now unused code

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,4 @@
 library(testthat)
 library(azuretest)
 
-is_windows <- identical(tolower(Sys.info()[["sysname"]]), "windows")
-
 test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
