49797a71b173d231edbda8bc6f6909697e913ba3
Jim Hester <james.f.hester@gmail.com>
2019-08-02 09:52:55 -0400

    Always use the MultiReporter

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,8 +3,4 @@ library(azuretest)
 
 is_windows <- identical(tolower(Sys.info()[["sysname"]]), "windows")
 
-if (!is_windows && require("xml2")) {
-  test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
-} else {
-  test_check("azuretest")
-}
+test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
