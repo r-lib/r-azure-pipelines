ecd0ae2a1bdec14c50498877a32d711663864410
Jim Hester <james.f.hester@gmail.com>
2019-08-07 09:17:44 -0400

    Make sure tests don't fail if xml2 is not installed

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,8 @@
 library(testthat)
 library(azuretest)
 
-test_check("azuretest", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
+if (requireNamespace("xml2")) {
+  test_check("azuretest", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
+} else {
+  test_check("azuretest")
+}
--- tests/testthat/test-foo.R
+++ tests/testthat/test-foo.R
@@ -1,3 +1,7 @@
 test_that("foo works", {
   expect_equal(foo(), 1)
 })
+
+test_that("foo does not work", {
+  expect_equal(foo(), 2)
+})
