8e3e63e0824da623ac4e04c78b347c6df9c77800
Jim Hester <james.f.hester@gmail.com>
2019-08-19 11:31:37 -0400

    Show a test failure

--- tests/testthat/test-foo.R
+++ tests/testthat/test-foo.R
@@ -2,9 +2,9 @@ test_that("foo works", {
   expect_equal(foo(), 1)
 })
 
-#test_that("foo does not work", {
-  #expect_equal(foo(), 2)
-#})
+test_that("foo does not work", {
+  expect_equal(foo(), 2)
+})
 
 #test_that("bar errors", {
   #expect_equal(bar(1), 2)
